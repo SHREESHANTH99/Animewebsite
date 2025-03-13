4f32324825721d0a55f8e66ef5681be5ce5b9f38
SHREESHANTH99 <shreeshanthshetty@gmail.com>
2025-03-13 15:57:30 +0530

    new slider

--- main.js
+++ main.js
@@ -34,3 +34,30 @@ document.addEventListener("DOMContentLoaded", function () {
     console.error("Menu toggle or navigation ul not found");
   }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var anime_swiper = new Swiper(".anime_swiper", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    grabCursor: true,
+    breakpoints: {
+      320: { slidesPerView: 1, spaceBetween: 20 },
+      640: { slidesPerView: 2, spaceBetween: 20 },
+      1024: { slidesPerView: 3, spaceBetween: 20 },
+      1280: { slidesPerView: 4, spaceBetween: 20},
+    },
+  });
+});
