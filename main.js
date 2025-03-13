43ad1bde63bc3bbc952f09ef21a1cb5ccca45b44
SHREESHANTH99 <shreeshanthshetty@gmail.com>
2025-03-13 12:28:41 +0530

    new js

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var brand_swiper = new Swiper(".brand_swiper", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    grabCursor: true,
+    touchEventsTarget: "wrapper",
+    simulateTouch: true,
+    touchRatio: 1,
+    touchAngle: 45,
+    breakpoints: {
+      320: { slidesPerView: 2, spaceBetween: 10 },
+      640: { slidesPerView: 3, spaceBetween: 15 },
+      1024: { slidesPerView: 4, spaceBetween: 20 },
+    },
+  });
+  var menuToggle = document.querySelector(".menu-toggle");
+  var navigationUl = document.querySelector(".navigation ul");
 
+  if (menuToggle && navigationUl) {
+    menuToggle.addEventListener("click", function () {
+      navigationUl.classList.toggle("active");
+    });
+  } else {
+    console.error("Menu toggle or navigation ul not found");
+  }
+});
